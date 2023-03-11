c6635d51465ba13cda5838e41dc1f3012d6a4b8a
Karol Koltun <karolkoltundev@gmail.com>
2023-03-11 19:41:30 +0100

    International invoice changes

--- src/main/java/org/example/international/invoice/InternationalInvoiceRepository.java
+++ src/main/java/org/example/international/invoice/InternationalInvoiceRepository.java
@@ -3,4 +3,6 @@ package org.example.international.invoice;
 import org.springframework.data.repository.CrudRepository;
 
 interface InternationalInvoiceRepository extends CrudRepository<InternationalInvoice, Integer> {
+
+    Iterable<InternationalInvoice> findByInvoiceNumber(String invoiceNumber);
 }
--- src/main/java/org/example/international/invoice/InternationalInvoiceService.java
+++ src/main/java/org/example/international/invoice/InternationalInvoiceService.java
@@ -2,21 +2,34 @@ package org.example.international.invoice;
 
 import org.springframework.stereotype.Component;
 
+import java.util.stream.StreamSupport;
+
 @Component
 public class InternationalInvoiceService {
 
-    // todo use the new InternationalInvoiceRepository
+    private final InternationalInvoiceRepository invoiceRepository;
 
-    public InternationalInvoiceService() {
+    public InternationalInvoiceService(InternationalInvoiceRepository invoiceRepository) {
+        this.invoiceRepository = invoiceRepository;
     }
 
     public InternationalInvoice getInvoice(String invoiceNumber) {
-        return null;
+        return StreamSupport.stream(invoiceRepository.findByInvoiceNumber(invoiceNumber).spliterator(), false)
+                .findAny()
+                .orElse(null);
     }
 
-    public void addInvoice(InternationalInvoice internationalInvoice) throws InvoiceNumberAlreadyExistsException {
+    public void addInvoice(InternationalInvoice invoice) throws InvoiceNumberAlreadyExistsException {
+        boolean invoiceNumberExists = invoiceRepository.findByInvoiceNumber(invoice.getInvoiceNumber()).iterator().hasNext();
+        if (invoiceNumberExists) {
+            throw new org.example.invoice.InvoiceNumberAlreadyExistsException(invoice.getInvoiceNumber());
+        }
+
+        // todo check the added invoice
+        invoiceRepository.save(invoice);
     }
 
-    public void deleteInvoice(InternationalInvoice internationalInvoice) {
+    public void deleteInvoice(InternationalInvoice invoice) {
+        invoiceRepository.delete(invoice);
     }
 }
